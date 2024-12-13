cef0722638b786a4a0d5cf6b18a3b190f5725542
boujmaa6527 <sahlouneb@gmail.com>
2024-12-13 15:43:49 +0100

    menu with all fonction and with pagination

--- shop/src/main/java/fr/fms/ShopApplication.java
+++ shop/src/main/java/fr/fms/ShopApplication.java
@@ -18,7 +18,9 @@ import org.springframework.data.domain.PageRequest;
 
 @SpringBootApplication
 public class ShopApplication implements CommandLineRunner{
-
+	
+	//Varaiable  instance scanner/input to use in class
+	static Scanner sc = new Scanner(System.in); 
 
 	@Autowired
 	private CategoryRepository categoryRepository;
@@ -39,10 +41,10 @@ public class ShopApplication implements CommandLineRunner{
 		//		categoryRepository.save(category);
 		//		articleRepository.save(article);
 		//		
-		for(Article artic : articleRepository.findByBrand("Samsung")) {
-
-			System.out.println(artic);
-		}
+		//		for(Article artic : articleRepository.findByBrand("Samsung")) {
+		//
+		//			System.out.println(artic);
+		//		}
 		//		for(Article articBrandAndPrice : articleRepository.findByBrandAndPrice("Samsung", 250)) {
 		//			
 		//			System.out.println(articBrandAndPrice);
@@ -71,7 +73,7 @@ public class ShopApplication implements CommandLineRunner{
 		//		articleRepository.save(new Article("Hp", "Elitebook", 600, pc));
 		//		articleRepository.save(new Article("Asus", "proElite", 500, pc));
 		//		articleRepository.save(new Article("Toshiba", "AgainElite", 500, pc));
-				//Afficher les article par catégory
+		//Afficher les article par catégory
 		//		for(Article articleBycategoryId : articleRepository.findByCategoryId(1L)) {
 		//			System.out.println(articleBycategoryId);
 		//		}
@@ -85,18 +87,18 @@ public class ShopApplication implements CommandLineRunner{
 		//		System.out.println(article);
 
 		//Exercice1.3
-		for(Article articleAll: articleRepository.findByDescriptionAndMarque("Apple", "Ipad")) {
-			System.out.println(articleAll);
-		}
+		//		for(Article articleAll: articleRepository.findByDescriptionAndMarque("Apple", "Ipad")) {
+		//			System.out.println(articleAll);
+		//		}
 		//Exercie1.4 (delete number 11 of bdd article)
-		   //Optional<Article> article = articleRepository.findById(4L);
-				//if(article.isPresent()) {
-//					articleRepository.deleteById(4L);
-					//		System.out.println("Full success delete");
-//				}else {
-//							System.out.println("Id Non identifié en Base");
-//				}
-		
+		//Optional<Article> article = articleRepository.findById(4L);
+		//if(article.isPresent()) {
+		//					articleRepository.deleteById(4L);
+		//		System.out.println("Full success delete");
+		//				}else {
+		//							System.out.println("Id Non identifié en Base");
+		//				}
+
 
 		//Exercice1.5 update de l'article avec l'id
 		//		articleRepository.updateArticleById(7L, "hp", "ElitBook", 700);
@@ -109,7 +111,7 @@ public class ShopApplication implements CommandLineRunner{
 
 		Scanner sc = new Scanner(System.in);
 		while(true) {
-			System.out.println("Bienvenu dans notre applaication de gestion d'articles");
+			System.out.println("Bienvenu dans notre application de gestion d'articles");
 			System.out.println("1:Afficher tous les articles sans pagination");
 			System.out.println("2:Afficher tous les articles avec paginations");
 			System.out.println("*********************************************");
@@ -131,144 +133,217 @@ public class ShopApplication implements CommandLineRunner{
 			sc.nextLine();// toujour vider le cache
 			switch(value) {
 			case 1:
-				System.out.println("*********************************************");
-				System.out.println("Afficher tous les articles");
-				System.out.println(" IDENTIFIANT   BRAND   DESCRIPTION  PRICE CATEGORY");
-				for(Article articleAll: articleRepository.findAll()) {
-					System.out.println(articleAll);
-					};
+				toDisplayWithoutPagination();
 				System.out.println("*********************************************");
 				break;
 			case 2:
-//				int pageNumber =0;
-//				int PageSize = 5;
-//				PageRequest pageable = PageRequest.of(pageNumber, PageSize);
-//				Page<Article> page = articleRepository.findAll(pageable);
-//				for(Article articlePaginee : page.getContent()) {
-//					System.out.println("Id:"+articlePaginee.getId()+ "Brand: " + articlePaginee.getBrand()+ 
-//							"Description: "+articlePaginee.getDescription()+ " Price: "+ articlePaginee.getPrice());
-//				}
-				
-				System.out.println("*********************************************");
-				System.out.println("Afficher tous les articles");
-				
-				
-			case 3:
-				System.out.println("Ajouter un article");
-				System.out.println("Entrez une marque/brand");
-				String newBrand = sc.nextLine();
-				System.out.println("Entrez uen description");
-				String newDescription = sc.nextLine();
-				System.out.println("Entrez un  Prix");
-				double newPrice = sc.nextDouble();
-				System.out.println("Entrez numero de la catégorie");
-				Long newCategorie = sc.nextLong();
-				Optional<Category> categorie =  categoryRepository.findById(newCategorie);
-				Article newArticle = new Article(newBrand, newDescription,newPrice, categorie.get());
-				articleRepository.save(newArticle);
-				System.out.println("Opération Succes Full");
+				toDisplayWithPagination();
 				break;
-				
+			case 3:
+				toAddArticle();
+				break;
 			case 4:
-				System.out.println("Entrez l'Id de l'article");
-				Long idValue = sc.nextLong();
-				Optional<Article> article = articleRepository.findById(idValue);
-				if(article.isPresent()) {
-					Article ar = article.get();
-					//change methode toString to dispaly allArticle
-					System.out.println(ar);
-				}else {
-					System.out.println("Id Article Inconnu");
-				}
-				
+				toDisplayArticle();
 				break;
 			case 5:
-				System.out.println("Entrez l'Id de l'article");
-				Long idValueToDel = sc.nextLong();
-				articleRepository.deleteById(idValueToDel);
-				System.out.println("Opération Success Full");
+				toDeleteArticle();
 				break;
 			case 6:
-				System.out.println("Entrez l'Id de l'article a modifier");
-				Long idValueToModifie = sc.nextLong();
-				sc.nextLine();
-				Optional<Article> articleToModifie = articleRepository.findById(idValueToModifie);
-				if(articleToModifie.isPresent()) {
-					System.out.println(articleToModifie);
-					System.out.println("Entrez pour modifier marque/brand");
-					String newBrandMode = sc.nextLine();
-					sc.nextLine();
-					System.out.println("Entrez pour modifier description");
-					String newDescriptionMode = sc.nextLine();
-					System.out.println("Entrez pour modifier Prix");
-					double newPriceMode = sc.nextDouble();
-					articleRepository.updateArticleById(idValueToModifie, newBrandMode, newDescriptionMode, newPriceMode);
-					System.out.println("Opération Success Full");
-					break;
-				}else {
-					System.out.println("Id Article Inconnu");
-				}
+				toModifieArticle();
 				break;
 			case 7:
-				System.out.println("Ajouter une nouvelle catégorie");
-				System.out.println("Entrez nom de la catégorie");
-				String nameCategorie = sc.nextLine();
-				categoryRepository.save(new Category(nameCategorie));
-				System.out.println("Opération Success Full");
+				toAddCategorie();
 				break;
-				
+
 			case 8:
-				System.out.println("Afficher une catégorie");
-				System.out.println("Entrez ID de la catégorie");
-				Long idValueCat = sc.nextLong();
-				Optional<Category> category = categoryRepository.findById(idValueCat);
-				if(category.isPresent()) {
-					System.out.println(category);
-				}else {
-					System.out.println("Id category Inconnu");
-				}
+				toDisplayCategorie();
 				break;
-			case 9:
-				System.out.println("Entrez l'Id de la catégory");
-				Long idCatToDel = sc.nextLong();
-				categoryRepository.deleteById(idCatToDel);
-				System.out.println("Opération Success Full");
+			case 9:	
+				toDeleteCategorie();
 				break;
-				
+
 			case 10:
-				System.out.println("Entrez l'Id de la catégorie a modifier");
-				Long idCatToModifie = sc.nextLong();
-				sc.nextLine();
-				Optional<Category> categoryToMode = categoryRepository.findById(idCatToModifie);
-				if(categoryToMode.isPresent()) {
-					System.out.println("Entrez name pour modifier la catégorie");
-					String newCatName = sc.nextLine();
-					categoryRepository.updateCategoryById(categoryToMode.get().getId(), newCatName);
-					System.out.println("Opération Success Full");
-					
-				}else {
-					System.out.println("Id category Inconnu");
-				}
+				toUpdateCategorie();
 				break;
 			case 11:
-				System.out.println("Entrez id de la  catégorie");
-				Long IdCateArticle = sc.nextLong();
-				Optional<Category> categoryToDisplay = categoryRepository.findById(IdCateArticle);
-				for(Article articleBycategoryId : articleRepository.findByCategoryId(categoryToDisplay.get().getId())) {
-					if(categoryToDisplay.isPresent()) {
-						System.out.println(articleBycategoryId);
-					}else {
-						System.out.println("Id category Inconnu");
-					}
-							
-				}
+				ToAllArticleByCategorie();
 				break;
-				
+			case 12 :
+				System.out.println("Retour au Menu");
+				break;
 
-				}
-
+			}
+			
 		}
 	}
+		public void toDisplayWithoutPagination() {
+			System.out.println("*********************************************");
+			System.out.println("Afficher tous les articles");
+			System.out.println(" IDENTIFIANT   BRAND   DESCRIPTION  PRICE CATEGORY");
+			for(Article articleAll: articleRepository.findAll()) {
+				System.out.println(articleAll);
+			};
+		}
+		public void toDisplayWithPagination() {
+			int pageNumber = 0;
+			int PageSize = 5;
+			PageRequest pageable;
+			Page<Article> page;
+			while(true) {
+				// p inside boucle because infinity boucle
+				pageable = PageRequest.of(pageNumber, PageSize);
+				page = articleRepository.findAll(pageable);
+				System.out.println("Page"+(pageNumber +1 ) +" of "+page.getTotalPages());
+				System.out.println("total Element:"+ page.getTotalElements());
+				page.getContent().forEach(article -> System.out.println(article));
+				System.out.println("\nOption: ");
+				System.out.println("1. Next page");
+				System.out.println("2. Previous page");
+				System.out.println("3. Exit");
+	
+	
+				int choice = sc.nextInt();
+				if(choice == 1 && page.hasNext()) {
+					pageNumber++;
+				}else if (choice == 2 && pageNumber > 0) {
+					pageNumber--;
+				}else if (choice == 3) {
+					break;
+				}else if(choice == 4) {
+					System.out.println("\nOption");
+					System.out.println("1. Next page");
+					System.out.println("2. Previous page");
+					System.out.println("3. Exit");
+				}
+	
+				else {
+					System.out.println("Error choice or no more pages");
+				}
+			}
+	}
+		public void toAddArticle() {
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Ajouter un article");
+			System.out.println("Entrez une marque/brand");
+			String newBrand = sc.nextLine();
+			System.out.println("Entrez uen description");
+			String newDescription = sc.nextLine();
+			System.out.println("Entrez un  Prix");
+			double newPrice = sc.nextDouble();
+			System.out.println("Entrez numero de la catégorie");
+			Long newCategorie = sc.nextLong();
+			Optional<Category> categorie =  categoryRepository.findById(newCategorie);
+			Article newArticle = new Article(newBrand, newDescription,newPrice, categorie.get());
+			articleRepository.save(newArticle);
+			System.out.println("Opération Succes Full");
+		}
+		public void toDisplayArticle() {
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Entrez l'Id de l'article");
+			Long idValue = sc.nextLong();
+			Optional<Article> article = articleRepository.findById(idValue);
+			if(article.isPresent()) {
+				Article ar = article.get();
+				//change methode toString to dispaly allArticle
+				System.out.println(ar);
+			}else {
+				System.out.println("Id Article Inconnu");
+			}
+		}
+		public void toDeleteArticle() {
+			System.out.println("Entrez l'Id de l'article");
+			Long idValueToDel = sc.nextLong();
+			articleRepository.deleteById(idValueToDel);
+			System.out.println("Opération Success Full");
+		}
+		public void toModifieArticle() {
+			System.out.println("Entrez l'Id de l'article a modifier");
+			Long idValueToModifie = sc.nextLong();
+			sc.nextLine();
+			Optional<Article> articleToModifie = articleRepository.findById(idValueToModifie);
+			if(articleToModifie.isPresent()) {
+				System.out.println(articleToModifie);
+				System.out.println("Entrez pour modifier marque/brand");
+				String newBrandMode = sc.nextLine();
+				sc.nextLine();
+				System.out.println("Entrez pour modifier description");
+				String newDescriptionMode = sc.nextLine();
+				System.out.println("Entrez pour modifier Prix");
+				double newPriceMode = sc.nextDouble();
+				articleRepository.updateArticleById(idValueToModifie, newBrandMode, newDescriptionMode, newPriceMode);
+				System.out.println("Opération Success Full");
+				
+			}else {
+				System.out.println("Id Article Inconnu");
+			}
+		}
+		public void toAddCategorie() {
+			System.out.println("Ajouter une nouvelle catégorie");
+			System.out.println("Entrez nom de la catégorie");
+			String nameCategorie = sc.nextLine();
+			categoryRepository.save(new Category(nameCategorie));
+			System.out.println("Opération Success Full");
+		}
+		public void toDisplayCategorie() {
+			System.out.println("Afficher une catégorie");
+			System.out.println("Entrez ID de la catégorie");
+			Long idValueCat = sc.nextLong();
+			Optional<Category> category = categoryRepository.findById(idValueCat);
+			if(category.isPresent()) {
+				System.out.println(category);
+			}else {
+				System.out.println("Id category Inconnu");
+			}
+		}
+		public void toDeleteCategorie() {
+			System.out.println("Entrez l'Id de la catégory");
+			Long idCatToDel = sc.nextLong();
+			//to verif if category exist
+			Optional<Category> categoryTodel = categoryRepository.findById(idCatToDel);
+			if(categoryTodel.isPresent()) {
+				System.out.println("Are you sur to delete? Oui/Non");
+				sc.nextLine();
+
+				String valDel = sc.nextLine();
+				if(valDel.equalsIgnoreCase("Oui")) {
+					categoryRepository.deleteById(categoryTodel.get().getId());
+					System.out.println("Opération Success Full");
+				}else {
+					System.out.println("Retour au Menu");
+				}
+
+			}else {
+				System.out.println("Id category Inconnu, Retour au Menu");
+			}
+		}
+		public void toUpdateCategorie() {
+			System.out.println("Entrez l'Id de la catégorie a modifier");
+			Long idCatToModifie = sc.nextLong();
+			sc.nextLine();
+			Optional<Category> categoryToMode = categoryRepository.findById(idCatToModifie);
+			if(categoryToMode.isPresent()) {
+				System.out.println("Entrez name pour modifier la catégorie");
+				String newCatName = sc.nextLine();
+				categoryRepository.updateCategoryById(categoryToMode.get().getId(), newCatName);
+				System.out.println("Opération Success Full");
+
+			}else {
+				System.out.println("Id category Inconnu");
+			}
+		}
+		public void ToAllArticleByCategorie() {
+			System.out.println("Entrez id de la  catégorie");
+			Long IdCateArticle = sc.nextLong();
+			Optional<Category> categoryToDisplay = categoryRepository.findById(IdCateArticle);
+			for(Article articleBycategoryId : articleRepository.findByCategoryId(categoryToDisplay.get().getId())) {
+				if(categoryToDisplay.isPresent()) {
+					System.out.println(articleBycategoryId);
+				}else {
+					System.out.println("Id category Inconnu");
+				}
+
+			}
+		}
 }
 
 
--- shop/src/main/java/fr/fms/entities/Article.java
+++ shop/src/main/java/fr/fms/entities/Article.java
@@ -1,5 +1,4 @@
 package fr.fms.entities;
-
 import java.io.Serializable;
 import java.util.List;
 
